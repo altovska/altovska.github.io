f2fa46fab34d97faa340b2afeccb7732bca33183
Altovska Karina <altovskaya88@gmail.com>
2018-02-20 22:52:16 +0200

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+$(function() {
+ $("#text").delay(2700).fadeTo( "slow", 1 );
+  $("#nine").delay(2700).fadeTo( "slow", 1 );
+  $("#two").delay(2700).fadeTo( "slow", 1 );
+  $("#one").delay(2700).fadeTo( "slow", 1 );
+  $("#seven").delay(2700).fadeTo( "slow", 1 );
+  $("#four").delay(2700).fadeTo( "slow", 1 );
+});
